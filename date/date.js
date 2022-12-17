a01f52a1c35ca255b944e271530f8bfaa91ef35c
Admin <kavyak6999@gmail.com>
2022-12-17 16:16:02 +0530

    date methods

---
+++ date/date.js
@@ -0,0 +1,28 @@
+//-------get--------
+//var x = Date();
+//var x = Date.now();
+//var x = new Date("july 25, 1995 23:15:30"); //returns a date object with the current date and time
+// var x = new Date();
+//document.write(x);
+//document.write(x.getFullYear());
+//document.write(x.getMonth());
+//document.write(x.getDate());
+//document.write(x.getDay()); // Sunday - Saturday : 0 - 6
+//document.write(x.getHours());
+//document.write(x.getMinutes());
+//document.write(x.getSeconds());
+//document.write(x.getMilliseconds());
+//document.write(x.getTime());
+
+//-------set--------
+var x = new Date();
+x.setDate(11);
+//x.setMonth(0);
+//x.setFullYear(2020);
+//x.setHours(20);
+//x.setMinutes(40);
+//x.setSeconds(30);
+//x.setMilliseconds(456);
+x.getDate();
+document.write(x);
+
